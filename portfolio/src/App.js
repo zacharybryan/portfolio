ac946309bb12da749f2342da8eb331d88887b0a0
Zach Bryan <zacharybryan@icloud.com>
2020-11-24 23:40:39 -0800

    constuctor for nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+class App extends React.Component { 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Zach Bryan',
+      headerLinks: [
+        { title: 'Home', path: '/'},
+        { title: 'About', path: '/about'},
+        { title: 'Contact', path: '/contact'},
+      ]
+    }
+  }
   render() {
     return (
       <div>Hello React</div>
