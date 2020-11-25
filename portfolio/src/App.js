232428bb6efec2a1032badfa18466c45d08e7e69
Zach Bryan <zacharybryan@icloud.com>
2020-11-24 23:33:30 -0800

    succesfull hello react

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello React</div>
+    );
+  }
 }
 
 export default App;
