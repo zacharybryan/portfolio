c6ae8c5347b95ef21f8e6af43ae04b1b660615b1
Zach Bryan <zacharybryan@icloud.com>
2020-11-25 00:07:08 -0800

    added basic NavBar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import './App.css';
 
-class App extends React.Component { 
+class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       title: 'Zach Bryan',
       headerLinks: [
-        { title: 'Home', path: '/'},
-        { title: 'About', path: '/about'},
-        { title: 'Contact', path: '/contact'},
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
       ],
       home: {
         title: "Zach Bryan",
         subTitle: "Never give up!",
-        subText:"Be Kind."
+        subText: "Be Kind."
       },
       about: {
         title: 'About Me'
@@ -30,7 +32,11 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0">
-          <p>Hi from React</p>
+        
+          <Navbar className="border-bottom">
+            <NavbarBrand>Zachary Bryan</NavbarBrand>
+          </Navbar>
+
         </Container>
       </Router>
     );
