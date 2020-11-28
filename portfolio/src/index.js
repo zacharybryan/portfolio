dde1954659e2fa5975af2be4b35ae8a516435d5b
Zach Bryan <zacharybryan@icloud.com>
2020-11-28 13:08:36 -0800

    import bootstrap into index.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
