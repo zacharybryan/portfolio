70549cfd5be7c68c200b029acd32d02847ada012
Zach Bryan <zacharybryan@icloud.com>
2020-11-28 17:26:36 -0800

    created selected method for cards and map to collect card data

--- portfolio/src/components/Carousel.js
+++ portfolio/src/components/Carousel.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import MealPlanner from '../assets/images/welcomepage.png';
+import Seeds from '../assets/images/seeds.png';
+import EmployeeSummary from '../assets/images/Employee-Summary.jpg';
+import PasswordGenerator from '../assets/images/password-gen.png'
+import FitnessTracker from '../assets/images/charts.png'
+import BudgetTracker from '../assets/images/budgetTracker.png'
 
 class Carousel extends React.Component {
     constructor(props) {
@@ -9,7 +15,7 @@ class Carousel extends React.Component {
                     id: 0,
                     title: 'Meal Planner',
                     subTitle: 'Find recipes, schedule meal times, and create grocery lists',
-                    imgSrc: '../assets/images/welcomepage.png',
+                    imgSrc: MealPlanner,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://tbsanders5.github.io/mealplanner/',
                     selected: false
@@ -18,7 +24,7 @@ class Carousel extends React.Component {
                     id: 1,
                     title: 'Seeds',
                     subTitle: 'Elevate your mood!',
-                    imgSrc: '../assets/images/seeds.png',
+                    imgSrc: Seeds,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://hidden-caverns-84141.herokuapp.com/',
                     selected: false
@@ -27,7 +33,7 @@ class Carousel extends React.Component {
                     id: 2,
                     title: 'Employee Summary',
                     subTitle: 'Create rosters and manage your team',
-                    imgSrc: '../assets/images/Employee-Summary.jpg',
+                    imgSrc: EmployeeSummary,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://github.com/zacharybryan/employeeSummary',
                     selected: false
@@ -36,7 +42,7 @@ class Carousel extends React.Component {
                     id: 3,
                     title: 'Password Generator',
                     subTitle: 'Create custom passwords',
-                    imgSrc: '../assets/images/password-gen.png',
+                    imgSrc: PasswordGenerator,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://github.com/zacharybryan/js-password-generator',
                     selected: false
@@ -45,7 +51,7 @@ class Carousel extends React.Component {
                     id: 4,
                     title: 'Fitness Tracker',
                     subTitle: 'Track your workouts',
-                    imgSrc: '../assets/images/charts.png',
+                    imgSrc: FitnessTracker,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://workout-tracker-zb.herokuapp.com/?id=5fbd5eea4fad1d00175def44',
                     selected: false
@@ -54,15 +60,40 @@ class Carousel extends React.Component {
                     id: 5,
                     title: 'Budget Tracker',
                     subTitle: 'This App works on and off line. Create an app shortcut on your smart phone',
-                    imgSrc: '../assets/images/budgetTracker.png',
+                    imgSrc: BudgetTracker,
                     imgAlt:"Photo of Home Screen",
                     link: 'https://budget-trackerzb.herokuapp.com/',
                     selected: false
-                }
+                },
             ]
 
         }
     }
+// Toggle
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+        // if its selected set it to false if not selected set it to true to create a toggle
+        items[id].selected = items[id].selected ? false : true;
+
+        // only one selected at one time 
+        items.forEach(item => {
+            if (item.id !== id) {
+                item.selected = false;
+            }
+        });
+        this.setState({
+            items
+        });
+    }
+
+    // maps to items
+    makeItem = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
     render() {
         return(
             <p>works</p>
