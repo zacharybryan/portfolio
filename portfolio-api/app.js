cb591ed6e16eeb975605cb53a6b21c0a7e028a37
Zach Bryan <zacharybryan@icloud.com>
2020-11-28 22:33:07 -0800

    prepping for deployment

--- portfolio-api/app.js
+++ portfolio-api/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sendGrid = require('@sendgrid/mail');
+const { setApiKey } = require('@sendgrid/mail');
+
+const PORT = process.env.PORT || 3030;
 const app = express();
-const PORT = 8080; 
+
 
 app.use(bodyParser.json());
+
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -22,8 +26,31 @@ app.get('/api', (req, res) => {
 app.post('/api/email', (req, res, next) => {
     console.log(req.body);
 
-    sendGrid.setApiKey('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');
+    sendGrid.setApiKey('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')
+
+    const msg = {
+        to: 'zacharybryan@icloud.com',
+        from: 'zacharybryan@icloud.com',
+        subject: 'Portfolio Contact',
+        text: req.body.message,
+    }
+
+    sendGrid
+        .send(msg)
+        .then(result => {
+
+            res.status(200).json({
+                success: true
+            });
+        })
+    .catch(err => {
+        console.log('error:', err);
+        res.status(401).json({
+            success: false
+        })
+    })
 });
+
 
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
